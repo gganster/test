008c78b75f76ee339b47daf979327efddbf0bfec
gganster <guilian.ganster@wisoftify.fr>
2023-09-04 08:25:38 +0200

    test

---
+++ 02_ternaire.js
@@ -0,0 +1,11 @@
+
+/**
+ * retourne la valeur la plus grande des deux paramètres
+ * 
+ * Contraintes:
+ *    - utiliser l'opérateur ternaire (if interdit)
+ */
+function ternaire(a , b) {
+}
+
+module.exports = ternaire;
